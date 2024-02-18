e830b2539471356c67d584a98e98e6cdf924dddf
assassinscreed2 <ankursolanki1537@gmail.com>
2024-02-18 22:58:59 +0530

    Add functionalities to the App

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,24 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+    Button clickButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.mybutton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"You clicked here",Toast.LENGTH_SHORT).show();
-                button.setPadding(12, 10, 0, 9);
-            }
-        });
+        editText = findViewById(R.id.edittext);
+        textView = findViewById(R.id.textview);
+        clickButton = findViewById(R.id.clickbutton);
+
+
+
+        clickButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int getNumber = Integer.parseInt(editText.getText().toString());
+
+                        int pound = convertKiloToPound(getNumber);
+
+                        textView.setText(""+pound);
+                    }
+                }
+        );
 
     }
+
+    private int convertKiloToPound(int kilo){
+        return 2*kilo;
+    }
+
+
 }
